d6e030479b948f9ede484b610a768f04a51dc44b
WebMagic Online 1 <webmagic.online1@gmail.com>
2025-01-05 10:18:13 +0000

    changes authenication service port number

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const fetch = require('node-fetch');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
