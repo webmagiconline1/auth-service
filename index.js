4b5b6039ebb52584cd56a33f1c6426eff550a7c0
webmagiconline1 <78255428+webmagiconline1@users.noreply.github.com>
2025-01-05 09:21:21 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+const users = []; // This should be replaced with a database in a real application
+
+// Register a new user
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    users.push({ username, password: hashedPassword });
+    res.status(201).send('User registered');
+});
+
+// Login a user
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = users.find(u => u.username === username);
+    if (user && await bcrypt.compare(password, user.password)) {
+        const token = jwt.sign({ username }, 'secret_key', { expiresIn: '1h' });
+        res.json({ token });
+    } else {
+        res.status(401).send('Invalid credentials');
+    }
+});
+
+// Middleware to authenticate token
+const authenticateToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    if (!token) return res.sendStatus(401);
+
+    jwt.verify(token, 'secret_key', (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+// Protected route
+app.get('/profile', authenticateToken, (req, res) => {
+    res.json({ message: `Welcome ${req.user.username}` });
+});
+
+app.listen(port, () => {
+    console.log(`Authentication service running on port ${port}`);
+});
